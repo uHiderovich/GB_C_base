ecfa44ba5ce27cf6e28027cc827b9582b1ee60fe
Артём Гапеевцев <agapeevtsev@yandex.ru>
2024-07-20 18:43:01 +0800

    ДЗ 9 Задание 6

--- HW9/task6.c
+++ HW9/task6.c
@@ -10,7 +10,41 @@
 Данные на выходе: 2
 */
 
-int main(int argc, char const* argv[]) {
-  /* code */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 1000
+
+int FindCountEndA(char* str) {
+  int counter = 0;
+
+  for (int i = 0; str[i] != '\0'; i++) {
+    if (str[i] == ' ' && str[i - 1] == 'a') {
+      counter++;
+    }
+  }
+
+  return counter;
+}
+
+int main() {
+  FILE* input = fopen("input.txt", "r");
+  FILE* output = fopen("output.txt", "w");
+
+  if (!input || !output) {
+    printf("Ошибка открытия файла.\n");
+    return 1;
+  }
+
+  char str[MAX_LEN + 1];
+
+  fgets(str, MAX_LEN, input);
+
+  fprintf(output, "%d\n", FindCountEndA(str));
+
+  fclose(input);
+  fclose(output);
+
   return 0;
 }
