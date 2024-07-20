9b0903c5d930bf2b45373b9c024f09c104f68be6
Артём Гапеевцев <agapeevtsev@yandex.ru>
2024-07-20 16:32:53 +0800

    ДЗ 9 Задание 5

--- HW9/task5.c
+++ HW9/task5.c
@@ -11,7 +11,54 @@
 Данные на выходе: beautiful
 */
 
-int main(int argc, char const* argv[]) {
-  /* code */
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 1000
+
+void FindLongestWord(char* str, char* longestWord) {
+  int maxLength = 0;
+  int currentWordLength = 0;
+  char currentStr[MAX_LEN + 1];
+
+  for (int i = 0; str[i] != '\0'; i++) {
+    char ch = str[i];
+    if (ch != ' ') {
+      currentStr[currentWordLength++] = ch;
+    }
+    else if (currentWordLength > maxLength) {
+      strcpy(longestWord, currentStr);
+      currentWordLength = 0;
+    }
+    else {
+      currentWordLength = 0;
+      memset(currentStr, 0, sizeof(currentStr));
+    }
+  }
+}
+
+int main() {
+  FILE* input = fopen("input.txt", "r");
+  FILE* output = fopen("output.txt", "w");
+
+  if (!input || !output) {
+    printf("Ошибка открытия файла.\n");
+    return 1;
+  }
+
+  char str[MAX_LEN + 1];
+  char longest_word[MAX_LEN + 1];
+
+  fgets(str, MAX_LEN, input);
+
+  str[strcspn(str, "\n")] = '\0';
+
+  FindLongestWord(str, longest_word);
+
+  fprintf(output, "%s\n", longest_word);
+
+  fclose(input);
+  fclose(output);
+
   return 0;
 }
